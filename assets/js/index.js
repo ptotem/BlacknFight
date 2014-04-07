5d182ef4399062026b2f16dbba20f3f8ec46ba46
RakeshRengaraj <rakesh.rengaraj@gmail.com>
2014-04-07 13:15:54 +0530

    initial html page done with css and js

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+    init();
+});
+
+function init(){
+    setTimeout(function(){
+        $('.block').hide();
+        $('#logo').show();
+        setTimeout(function(){
+            $('.block').hide();
+            $('#menu').show();
+        },1000)
+    },2000)
+
+    $('#start_game').click(function(){
+        $('.block').hide();
+        alert("Attack mode");
+    });
+
+}
+
+
+
+
+
